964ba6a586ce63ab545c69bd41c5cef0f72e8b78
joamag <joamag@gmail.com>
2014-04-23 11:19:01 +0100

    new inclusion

--- src/appier_extras/parts/admin/part.py
+++ src/appier_extras/parts/admin/part.py
@@ -39,7 +39,7 @@ __license__ = "GNU General Public License (GPL), Version 3"
 
 import appier
 
-import models
+from appier_extras.parts.admin import models
 
 class AdminPart(appier.Part):
     """
