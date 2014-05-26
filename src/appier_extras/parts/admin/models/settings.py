88f5be7211a0408b56f182dbdcf4ac15b750bb02
joamag <joamag@gmail.com>
2014-05-26 12:37:53 +0100

    new settings model

--- src/appier_extras/parts/admin/models/settings.py
+++ src/appier_extras/parts/admin/models/settings.py
@@ -19,6 +19,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Hive Appier Framework. If not, see <http://www.gnu.org/licenses/>.
 
+__author__ = "João Magalhães joamag@hive.pt>"
+""" The author(s) of the module """
+
 __version__ = "1.0.0"
 """ The version of the module """
 
@@ -34,10 +37,17 @@ __copyright__ = "Copyright (c) 2008-2014 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
-from . import account
-from . import base
-from . import settings
+import appier
 
-from .account import *
-from .base import *
-from .settings import *
+from appier_extras.parts.admin.models import base
+
+class Settings(base.Base):
+
+    meta = appier.field(
+        type = dict,
+        index = True
+    )
+
+    @classmethod
+    def get_settings(cls, *args, **kwargs):
+        return cls.singleton(*args, **kwargs)
