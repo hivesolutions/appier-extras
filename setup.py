0624f073d521baae7c6aaf4d977249c7cc3d99e8
joamag <joamag@gmail.com>
2016-09-02 17:14:05 +0100

    new version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "appier_extras",
-    version = "0.7.19",
+    version = "0.7.20",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Appier Framework Extra Elements",
@@ -58,6 +58,7 @@ setuptools.setup(
         "appier_extras.parts.admin.social",
         "appier_extras.parts.captcha",
         "appier_extras.parts.csfr",
+        "appier_extras.parts.opbeat",
         "appier_extras.test",
         "appier_extras.utils"
     ],
