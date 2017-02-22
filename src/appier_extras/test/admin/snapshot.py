10ec3dd66a3c8f24f725b146b09520f3a8269510
joamag <joamag@gmail.com>
2017-02-22 13:00:37 +0000

    new session class

--- src/appier_extras/test/admin/snapshot.py
+++ src/appier_extras/test/admin/snapshot.py
@@ -47,7 +47,7 @@ from . import mock
 class SnapshotTest(unittest.TestCase):
 
     def setUp(self):
-        self.app = appier.App()
+        self.app = appier.App(session_c = appier.MemorySession)
         self.app._register_models_m(mock, "Mocks")
 
     def tearDown(self):
