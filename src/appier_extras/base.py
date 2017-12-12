333b3251da160230c0cbf3242cfb30a2d31b1767
joamag <joamag@hive.pt>
2017-12-12 15:32:03 +0000

    new version

--- src/appier_extras/base.py
+++ src/appier_extras/base.py
@@ -41,6 +41,6 @@ NAME = "appier_extras"
 """ The name to be used to describe the library while working
 on its own environment, this is just a descriptive value """
 
-VERSION = "0.13.28"
+VERSION = "0.13.29"
 """ The version of the library that is currently installed
 this value may be used for debugging/diagnostic purposes """
