9f0d28a74e6a6d351ec3f3a41238bca17f6f2632
joamag <joamag@gmail.com>
2019-11-22 20:56:26 +0000

    New version 0.19.48

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "appier_extras",
-    version = "0.19.47",
+    version = "0.19.48",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Appier Framework Extra Elements",
