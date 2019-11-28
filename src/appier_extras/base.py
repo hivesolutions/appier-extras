3b019adda23e3277d4a2b3f8c37c6c801b752d0e
joamag <joamag@gmail.com>
2019-11-28 19:34:31 +0000

    New version 0.19.54

--- src/appier_extras/base.py
+++ src/appier_extras/base.py
@@ -41,6 +41,6 @@ NAME = "appier_extras"
 """ The name to be used to describe the library while working
 on its own environment, this is just a descriptive value """
 
-VERSION = "0.19.53"
+VERSION = "0.19.54"
 """ The version of the library that is currently installed
 this value may be used for debugging/diagnostic purposes """
