51d56f3de9aba977c4fb505de95971e7a37d2372
joamag <joamag@gmail.com>
2019-11-28 19:34:58 +0000

    Fixed cast issue

--- src/appier_extras/parts/admin/models/base.py
+++ src/appier_extras/parts/admin/models/base.py
@@ -468,8 +468,8 @@ class Base(appier.Model):
     @classmethod
     def _inlinify_premailer(cls, data, *args, **kwargs):
         premailer = appier.import_pip("premailer")
-        keep_style_tags = appier.conf("PREMAILER_KEEP_TAGS", True)
-        strip_important = appier.conf("PREMAILER_STRIP_IMPORTANT", False)
+        keep_style_tags = appier.conf("PREMAILER_KEEP_TAGS", True, cast = bool)
+        strip_important = appier.conf("PREMAILER_STRIP_IMPORTANT", False, cast = bool)
         keep_style_tags = kwargs.get("keep_style_tags", keep_style_tags)
         strip_important = kwargs.get("strip_important", strip_important)
         logging_level = kwargs.get(
