2fcebeb2c1517c09527091557f99cc4e902940ad
João Magalhães <joamag@gmail.com>
2021-01-14 13:30:12 +0000

    version: 0.21.3

--- src/appier_extras/base.py
+++ src/appier_extras/base.py
@@ -41,6 +41,6 @@ NAME = "appier_extras"
 """ The name to be used to describe the library while working
 on its own environment, this is just a descriptive value """
 
-VERSION = "0.21.2"
+VERSION = "0.21.3"
 """ The version of the library that is currently installed
 this value may be used for debugging/diagnostic purposes """
