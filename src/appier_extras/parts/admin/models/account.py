9edb73d4f876e139e2f43824483c8e6a4efd6914
João Magalhães <joamag@gmail.com>
2025-01-05 12:09:04 +0000

    chore: conditional support for new image call
    This is required because of a barcode API change.

--- src/appier_extras/parts/admin/models/account.py
+++ src/appier_extras/parts/admin/models/account.py
@@ -934,6 +934,8 @@ class Account(base.Base, authenticable.Authenticable):
         code.add_data(self.otp_uri)
         code.make(fit=True)
         image = code.make_image(fill_color="black", back_color="white")
+        if hasattr(image, "new_image"):
+            image = image.new_image()
         buffer = appier.legacy.BytesIO()
         image.save(buffer, format="PNG")
         image_data = buffer.getvalue()
